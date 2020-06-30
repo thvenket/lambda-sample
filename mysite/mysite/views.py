44ab44c07bf49549e78b62cd2db50f1cf844891e
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-30 08:35:26 -0700

    add text file .

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 # Put Standard Library Imports Here:
 import json
 import traceback
+import os
 
 # Put Third Party/Django Imports Here:
 # from django.contrib.auth.decorators import permission_required
@@ -13,5 +14,9 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'my demo of build for iot prject ! api-gateway demo !' 
+	title = 'lambda api-gateway demo !'
+	data_file = os.path.append(os.path.dirname(os.path.abspath(__file__)) , "data.txt")
+	with open (data_file, "r") as myfile:
+    	data = myfile.readlines()
+	response = "{0}<br>{1}".format(title, data)
 	return HttpResponse(response, content_type="text/html")
